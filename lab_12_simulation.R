c22e972ab614bb139992d5d10eb216fca7593029
Neha Srivastava <nehasrivastava@Nehas-MBP.wv.cc.cmu.edu>
2018-04-17 13:05:24 -0400

    model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,3 +3,15 @@ generate_data = function(n,p) {
   responses = c(rnorm(n,0,1))
   return(list(covariates=covariates, responses =responses))
 }
+
+
+model_select = function(covariates, responses, cutoff) {
+  lm = lm(responses~covariates)
+  retain = covariates[which(lm$coefficients[,"Pr(>|t|)"] <= cutoff)]
+  lm2 = lm(responses~retain)
+  if (length(retain) == 0) {
+    return(c())
+  } else {
+    return(lm2$coefficients[,"Pr(>|t|)"])
+  }
+}
